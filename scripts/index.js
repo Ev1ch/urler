1ca3e011a877b3e8f5af3fdd672b79e1a6deb8fe
Vlad Zusko <vladzvu11@gmail.com>
2022-08-18 00:31:42 +0300

    feat: add main logic

---
+++ scripts/index.js
@@ -0,0 +1,49 @@
+import { disableElement, enableElement } from './dom.js';
+import {
+  formElement,
+  inputElement,
+  buttonElement,
+  urlsElement,
+} from './elements.js';
+import { getObjectFromFormData } from './form-data.js';
+import { withPreventDefault } from './modifiers.js';
+import { createUrl } from './ui.js';
+import { Url } from './url.js';
+import { getId } from './utils.js';
+
+const history = [];
+
+function handleFormSubmit() {
+  try {
+    let { url } = getObjectFromFormData(new FormData(formElement));
+
+    if (!Url.isValid(url)) {
+      return;
+    }
+
+    url = url.trim();
+    const id = getId();
+
+    history.push({ id, url });
+
+    url = new Url(url);
+
+    urlsElement.prepend(createUrl(url, id));
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function handleInputChange(event) {
+  const url = event.target.value.trim();
+
+  if (!Url.isValid(url)) {
+    disableElement(buttonElement);
+    return;
+  }
+
+  enableElement(buttonElement);
+}
+
+formElement.addEventListener('submit', withPreventDefault(handleFormSubmit));
+inputElement.addEventListener('keyup', handleInputChange);
