77137984c9f12533b392245317717ce6d5ba95b7
Vlad Zusko <vladzvu11@gmail.com>
2022-09-25 00:03:48 +0300

    feat: add chage handler

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,14 @@
 import {
-  clearElement,
-  disableElement,
-  enableElement,
-  focusElement,
+    clearElement,
+    disableElement,
+    enableElement,
+    focusElement,
 } from './dom.js';
 import {
-  formElement,
-  inputElement,
-  buttonElement,
-  urlsElement,
+    formElement,
+    inputElement,
+    buttonElement,
+    urlsElement,
 } from './elements.js';
 import { getObjectFromFormData } from './form-data.js';
 import { withPreventDefault } from './modifiers.js';
@@ -19,44 +19,49 @@ import { getId } from './utils.js';
 const history = [];
 
 function handleFormSubmit() {
-  try {
-    let { url } = getObjectFromFormData(new FormData(formElement));
+    try {
+        let { url } = getObjectFromFormData(new FormData(formElement));
 
-    if (!Url.isValid(url)) {
-      return;
+        if (!Url.isValid(url)) {
+            return;
+        }
+
+        clearElement(inputElement);
+        disableElement(buttonElement);
+
+        url = url.trim();
+        const id = getId();
+
+        history.push({ id, url });
+
+        url = new Url(url);
+
+        urlsElement.prepend(createUrl(url, id));
+    } catch (error) {
+        console.error(error);
     }
-
-    clearElement(inputElement);
-    disableElement(buttonElement);
-
-    url = url.trim();
-    const id = getId();
-
-    history.push({ id, url });
-
-    url = new Url(url);
-
-    urlsElement.prepend(createUrl(url, id));
-  } catch (error) {
-    console.error(error);
-  }
 }
 
 function handleInputChange(event) {
-  const url = event.target.value.trim();
+    const url = event.target.value.trim();
 
-  if (!Url.isValid(url)) {
-    disableElement(buttonElement);
-    return;
-  }
+    if (!Url.isValid(url)) {
+        disableElement(buttonElement);
+        return;
+    }
 
-  enableElement(buttonElement);
+    enableElement(buttonElement);
 }
 
 function handleLoad() {
-  focusElement(inputElement);
+    focusElement(inputElement);
 }
 
 formElement.addEventListener('submit', withPreventDefault(handleFormSubmit));
 inputElement.addEventListener('keyup', handleInputChange);
+inputElement.addEventListener('change', handleInputChange);
 window.addEventListener('load', handleLoad);
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/scripts/worker.js');
+}
